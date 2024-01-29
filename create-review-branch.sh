a03af56bc0de5fe86a2d59552911f937f0c918ba
xryuseix <ryusei143.shootingstar@gmail.com>
2024-01-30 01:10:26 +0900

    2023年度秋テンプレートを追加

--- create-review-branch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-initial_commit_hash=$(git rev-list --max-parents=0 HEAD | tail -n 1)
-
-# 初期コミットを基にレビューブランチを作成
-git branch review "$initial_commit_hash"
-
-# レビューブランチをリモートにプッシュ
-git push origin review
